d65fe898c868de1fa27f9b12b3fbb6a526df5885
Szymon Szczyrbak <szymon.szczyrbak@op.pl>
2023-07-15 19:09:21 +0200

    Add dEQP-VK image results.

--- README.md
+++ README.md
@@ -7,5 +7,17 @@ The current goal is implementation of Vulkan Core 1.0 API in software.
 ## Goals
 - [x] vulkaninfo
 - [ ] vkcube
-  - [x] Added all entry points. 
-- [ ] dEQP tests
+  - [x] Entry points.
+  - [ ] Rendering.
+  - [ ] Presentation.
+- [ ] dEQP-VK tests
+  - [ ] dEQP-VK.api.*
+    - [x] dEQP-VK.api.info.*
+    - [ ] dEQP-VK.api.smoke.triangle
+      - [ ] Rendering.
+
+        ![alt text](assets/deqp-vk/dEQP-VK.api.smoke.triangle_Result.PNG)![alt text](assets/deqp-vk/dEQP-VK.api.smoke.triangle_Reference.PNG)![alt text](assets/deqp-vk/dEQP-VK.api.smoke.triangle_ErrorMask.PNG)
+    - [ ] dEQP-VK.api.object_management.single.buffer_view_uniform_r8g8b8a8_unorm
+  - [ ] dEQP-VK.memory.*
+    - [x] dEQP-VK.memory.requirements.*
+ 
--- test_suite/tests/integration_tests.rs
+++ test_suite/tests/integration_tests.rs
@@ -1,3 +1,6 @@
+use base64::Engine;
+use lazy_static::lazy_static;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 mod common;
@@ -34,18 +37,19 @@ fn check_exported_symbols() -> common::TestResult {
 
 fn run_executable(
     executable_path: &str,
-    current_dir: Option<&str>,
+    current_dir: Option<impl AsRef<Path>>,
     args: impl IntoIterator<Item = &'static str>,
+    callback: impl Fn(),
 ) -> common::TestResult {
     let icd_json_path = common::get_icd_json_path();
     let mut out = Command::new(executable_path);
-    let mut out = out
+    let out = out
         .env("VK_ICD_FILENAMES", icd_json_path)
         .env("VK_LOADER_DEBUG", "error,warn,debug") // error,warn,info,debug,layer,driver
         //.env("ICD_WAIT_FOR_DEBUGGER", "true")
         .env("RUST_LOG", "trace");
-    let mut out = if let Some(current_dir) = current_dir {
-        out.current_dir(std::fs::canonicalize(current_dir)?)
+    let out = if let Some(current_dir) = current_dir {
+        out.current_dir(current_dir)
     } else {
         out
     };
@@ -53,6 +57,7 @@ fn run_executable(
     let out = out.output()?;
     let stdout = String::from_utf8_lossy(&out.stdout);
     println!("stdout:\n{}", stdout);
+    callback();
     assert!(
         out.status.success(),
         "Didn't pass: stderr:\n{}",
@@ -68,89 +73,173 @@ fn run_executable(
 
 #[test]
 fn run_vulkaninfo() -> common::TestResult {
-    run_executable("vulkaninfo", None, [])
+    run_executable("vulkaninfo", None::<&str>, [], || {})
 }
 
 #[test]
 fn run_vkcube() -> common::TestResult {
-    run_executable("vkcube", None, [])
+    run_executable("vkcube", None::<&str>, [], || {})
 }
 
-fn run_deqp_vk(case_name: &'static str) -> common::TestResult {
+fn run_deqp_vk(case_name: &'static str, log_images: bool) -> common::TestResult {
+    let print_deqp_vk_image = || {
+        lazy_static! {
+            static ref IMAGE_OUTPUT_DIR: PathBuf = {
+                let path = std::fs::canonicalize(std::path::PathBuf::from(concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/../assets/deqp-vk"
+                )))
+                .unwrap();
+                std::fs::remove_dir_all(&path).unwrap();
+                std::fs::create_dir(&path).unwrap();
+                path
+            };
+        }
+        if log_images {
+            print_deqp_vk_image(case_name, &*IMAGE_OUTPUT_DIR);
+        }
+    };
     run_executable(
         "./deqp-vk",
-        std::env::var("VULKAN_CTS_PATH").ok().as_deref(),
+        Some(deqp_vk_dir()),
         [
-            "--deqp-log-images=disable",
+            if log_images {
+                "--deqp-log-images=enable"
+            } else {
+                "--deqp-log-images=disable"
+            },
             "--deqp-log-shader-sources=disable",
             "--deqp-terminate-on-fail=enable",
             "-n",
             case_name,
         ],
+        print_deqp_vk_image,
     )
+}
+
+fn deqp_vk_dir() -> PathBuf {
+    assert!(std::env::var("VULKAN_CTS_PATH").is_ok());
+    std::fs::canonicalize(std::env::var("VULKAN_CTS_PATH").unwrap()).unwrap()
 }
 
 #[ignore]
 #[test]
 fn run_deqp_vk_api_all() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.*")
+    run_deqp_vk("dEQP-VK.api.*", false)
+}
+
+fn print_deqp_vk_image(case_name: &str, image_output_dir: impl AsRef<Path>) {
+    let mut test_results_qpa_path = deqp_vk_dir();
+    test_results_qpa_path.push("TestResults.qpa");
+    let test_results_qpa = std::fs::read_to_string(test_results_qpa_path).unwrap();
+
+    lazy_static! {
+        static ref RE_IMAGE: regex::Regex =
+            regex::Regex::new(r#"(?s)<Image\sName="(?<name>.*?)"\sWidth="(?<width>.*?)"\sHeight="(?<height>.*?)"\sFormat="(?<format>.*?)"\sCompressionMode="(?<compression_mode>.*?)"\sDescription="(?<description>.*?)">(?<base64>.*?)<\/Image>"#).expect("regex");
+    }
+    for cap in RE_IMAGE.captures_iter(&test_results_qpa) {
+        let image = cap.name("base64").unwrap().as_str();
+        let image = image
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .map(|c| c as u8)
+            .collect::<Vec<_>>();
+        let image = base64::engine::general_purpose::STANDARD
+            .decode(&image)
+            .unwrap();
+
+        let mut path = PathBuf::from(image_output_dir.as_ref());
+        let filename = format!(
+            "{}_{}.{}",
+            case_name,
+            cap.name("name").unwrap().as_str(),
+            cap.name("compression_mode").unwrap().as_str()
+        );
+        path.push(filename);
+        std::fs::write(&path, image).unwrap();
+
+        viuer::print_from_file(
+            path,
+            &viuer::Config {
+                transparent: false,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+    }
 }
 
 #[test]
 fn run_deqp_vk_api_info() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.info.*")
+    run_deqp_vk("dEQP-VK.api.info.*", false)
 }
 
 #[test]
 fn run_deqp_vk_api_version_check_entry_points() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.version_check.entry_points")
+    run_deqp_vk("dEQP-VK.api.version_check.entry_points", false)
 }
 
 #[test]
 fn run_deqp_vk_api_device_init_create_device_basic() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.device_init.create_device.basic")
+    run_deqp_vk("dEQP-VK.api.device_init.create_device.basic", false)
 }
 
 #[ignore] // Pass, long execution.
 #[test]
 fn run_deqp_vk_api_device_init_create_instance_device_intentional_alloc_fail_basic(
 ) -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail.basic")
+    run_deqp_vk(
+        "dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail.basic",
+        false,
+    )
 }
 
 #[test]
 fn run_deqp_vk_api_device_init_create_device_unsupported_features_core() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.device_init.create_device_unsupported_features.core")
+    run_deqp_vk(
+        "dEQP-VK.api.device_init.create_device_unsupported_features.core",
+        false,
+    )
 }
 
 #[test]
 fn run_deqp_vk_api_object_management_single_buffer_view_uniform_r8g8b8a8_unorm(
 ) -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.object_management.single.buffer_view_uniform_r8g8b8a8_unorm")
+    run_deqp_vk(
+        "dEQP-VK.api.object_management.single.buffer_view_uniform_r8g8b8a8_unorm",
+        false,
+    )
 }
 
 #[test]
 fn run_deqp_vk_api_smoke_create_shader() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.smoke.create_shader")
+    run_deqp_vk("dEQP-VK.api.smoke.create_shader", false)
 }
 
 #[test]
 fn run_deqp_vk_api_smoke_triangle() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.api.smoke.triangle")
+    run_deqp_vk("dEQP-VK.api.smoke.triangle", true)
 }
 
-#[ignore]
+#[test]
+fn run_deqp_memory_all() -> common::TestResult {
+    run_deqp_vk("dEQP-VK.memory.*", false)
+}
+
 #[test]
 fn run_deqp_memory_requirements_all() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.memory.requirements.*")
+    run_deqp_vk("dEQP-VK.memory.requirements.*", false)
 }
 
 #[test]
 fn run_deqp_vk_memory_requirements_core_buffer_regular() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.memory.requirements.core.buffer.regular")
+    run_deqp_vk("dEQP-VK.memory.requirements.core.buffer.regular", false)
 }
 
 #[test]
 fn run_deqp_vk_memory_requirements_core_image_regular_tiling_linear() -> common::TestResult {
-    run_deqp_vk("dEQP-VK.memory.requirements.core.image.regular_tiling_linear")
+    run_deqp_vk(
+        "dEQP-VK.memory.requirements.core.image.regular_tiling_linear",
+        false,
+    )
 }
